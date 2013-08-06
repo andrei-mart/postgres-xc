e45c3a1f3cf058641a1b3df2a5b70e5938d39ff1
Nikhil Sontakke <nikkhils@gmail.com>
2013-08-06 10:14:01 +0530

    Don't send client disconnection messages immediately to the GTM if a Proxy is involved. The proxy puts it onto the pending list and processes all disconnections last. This avoids confusing the GTM

--- src/gtm/main/gtm_txn.c
+++ src/gtm/main/gtm_txn.c
@@ -159,7 +159,12 @@ GTM_GXIDToHandle(GlobalTransactionId gxid)
 	if (gtm_txninfo != NULL)
 		return gtm_txninfo->gti_handle;
 	else
+	{
+		ereport(WARNING,
+				(ERANGE, errmsg("No transaction handle for gxid: %d",
+								gxid)));
 		return InvalidTransactionHandle;
+	}
 }
 
 /*
@@ -1479,7 +1484,7 @@ ProcessBeginTransactionGetGXIDCommandMulti(Port *myport, StringInfo message)
 	/* GXID has been received, now it's time to get a GTM timestamp */
 	timestamp = GTM_TimestampGetCurrent();
 
-	end_gxid = start_gxid + txn_count;
+	end_gxid = start_gxid + (txn_count - 1);
 	if (end_gxid < start_gxid)
 		end_gxid += FirstNormalGlobalTransactionId;
 
